461a79c64ffe62f54c2e699c7d557d1e91504bfa
Tobias Lagander <tobias.lagander@smhi.se>
2020-10-30 09:13:44 +0100

    Run the main program with Rscript from an installed location by conda. Additional R packages not necessarily needed for the initial call. Could be a python main program (run.py).

--- src/main/app-resources/node_dataprep/run.R
+++ src/main/app-resources/node_dataprep/run.R
@@ -1,9 +1,9 @@
-#!/usr/bin/Rscript --vanilla --slave --quiet
+#!/opt/anaconda/envs/postprocessing-env/bin/Rscript --vanilla --slave --quiet
 
-# run 'which Rscript' in the terminal. If differs you may have to change the above line to e.g.
-#!/opt/anaconda/bin/Rscript --vanilla --slave --quiet
+##### #!/opt/anaconda/bin/Rscript --vanilla --slave --quiet
+##### #!/usr/bin/Rscript --vanilla --slave --quiet
 
-# R installed via yum, ignoring R in conda environment base.
+# run 'which Rscript' in the terminal. If the path differs you may have to change the first line above.
 # When using certain R packages, call Rscript in the appropriate conda environment with the appropriate source code file.
 
 print('start dataprep.....')
